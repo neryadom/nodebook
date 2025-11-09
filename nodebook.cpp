9ab7c4181164fe3167730118e578c75a3b988350
neryadom <henrichemla@outlook.com>
2025-11-10 01:49:31 +1300

    adding toString method and improving constructor

--- nodebook.cpp
+++ nodebook.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <string>
+
+
+class IdGenerator{
+	private:
+		static int idCounter;
+	public:
+		static int getId(){
+			int outputId = idCounter;
+			idCounter++;
+			return outputId;
+		}
+};
+
+
+int IdGenerator::idCounter = 0;
 
 class Node {
 	private:
 		int id;
 		int nodeByteSize;
-		int contentSize;
+		int contentByteSize;
 		int tagCount;
 		int linkCount;
 		std::string title;
@@ -16,8 +32,8 @@ class Node {
 		int getNodeByteSize(){
 			return nodeByteSize;
 		}
-		int getContentSize(){
-			return contentSize;
+		int getContentByteSize(){
+			return contentByteSize;
 		}
 		int getTagCount(){
 			return tagCount;
@@ -39,11 +55,29 @@ class Node {
 			content = newContent;
 		}
 
-		Node(std::string t, std::string c): title(t), content(c) {}
-		
+		Node(std::string t, std::string c) : 
+			id(IdGenerator::getId()),
+			nodeByteSize(sizeof(Node) + static_cast<int>(t.size()) + static_cast<int>(c.size())),
+			contentByteSize(static_cast<int>(c.size())),
+			tagCount(0), 
+			linkCount(0), 
+			title(t), 
+			content(c) {}
+
+		void toString(){
+			std::cout << "\nDETAILS OF NODE " << id << "\n"
+				<< "Node byte size: " << nodeByteSize << "\n"
+				<< "Content byte size: " << contentByteSize << "\n"
+				<< "Title: " << title << "\n"
+				<< "Content: " << content << "\n" << std::endl;
+		}
 };
 
 
 int main(){
+	Node n0("First Node", "The contents of the first node are vast and plentiful");
+	Node n1("Second Node", "I would like to talk about the merits of tables");
+	n0.toString();
+	n1.toString();
 	return 0;
 }
